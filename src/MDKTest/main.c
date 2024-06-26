75b2ccd393c6a03e2a5933cfd8f6eabfcc8f5c82
circl <circl.lastname@gmail.com>
2024-06-26 13:20:01 +0200

    MDKTest: Rewrite for quit event testing

--- src/MDKTest/main.c
+++ src/MDKTest/main.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #include <MDK/Application.h>
-#include <MDK/BackgroundTask.h>
 
-void backgroundEventsReceiver(MDK_Event* event) {
-  puts("received my event");
-  usleep(1000000);
-}
-
-void backgroundEventsTest(void* unused) {
-  puts("hello from thread");
-  
-  for (unsigned i = 0; i < 10; i++) {
-    MDK_Event* myEvent = malloc(sizeof(MDK_Event));
-    *myEvent = (MDK_Event){
-      .target = backgroundEventsReceiver,
-      .callback = (MDK_Event_Target)free,
-    };
-    
-    MDK_Application_sendEvent(myEvent);
-    puts("sent an event");
-  }
-}
+unsigned quitRequestCount = 0;
 
 void onStart(MDK_Application_StartEvent* event) {
-  puts("hello");
+  puts("Application has started");
+  printf("My name is %s and I have %i arguments\n", event->argv[0], event->argc);
+}
+
+void onQuitRequest(MDK_Event* event) {
+  quitRequestCount++;
   
-  MDK_BackgroundTask* backgroundEventsTask = MDK_BackgroundTask_create(backgroundEventsTest, NULL);
+  printf("Received quit request no. %u\n", quitRequestCount);
+  
+  if (quitRequestCount == 5) {
+    puts("Fine! Quitting");
+    exit(0);
+  }
 }
 
 int main(int argc, char* argv[]) {
   MDK_Application_onStart(onStart);
+  MDK_Application_onQuitRequest(onQuitRequest);
   return MDK_Application_start(argc, argv);
 }
