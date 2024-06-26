4d7926db3896f57312ab5e1dfb9cf95fe48c2ef8
circl <circl.lastname@gmail.com>
2024-06-26 15:55:47 +0200

    libMDK: Add getIsRunning to BackgroundTask

--- include/MDK/BackgroundTask.h
+++ include/MDK/BackgroundTask.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <stdbool.h>
+
 typedef struct MDK_BackgroundTask_struct MDK_BackgroundTask;
 
 MDK_BackgroundTask* MDK_BackgroundTask_create(void (*taskMain)(void* data), void* data);
 void MDK_BackgroundTask_destroy(MDK_BackgroundTask* task);
+
+bool MDK_BackgroundTask_getIsRunning(MDK_BackgroundTask* task);
 
 void MDK_BackgroundTask_stop(MDK_BackgroundTask* task);
--- src/libMDK/BackgroundTask.c
+++ src/libMDK/BackgroundTask.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <stdatomic.h>
 #include <stdlib.h>
 
 #include <MDK/BackgroundTask.h>
@@ -7,19 +8,23 @@ struct MDK_BackgroundTask_struct {
   pthread_t thread;
   void (*main)(void* data);
   void* data;
+  _Atomic bool isRunning;
 };
 
 static void* threadMain(void* task_raw) {
   MDK_BackgroundTask* task = task_raw;
   
+  task->isRunning = true;
   pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
-  
   task->main(task->data);
+  
+  task->isRunning = false;
   return NULL;
 }
 
 MDK_BackgroundTask* MDK_BackgroundTask_create(void (*taskMain)(void* data), void* data) {
   MDK_BackgroundTask* task = malloc(sizeof(MDK_BackgroundTask));
+  atomic_init(&task->isRunning, false);
   
   task->main = taskMain;
   task->data = data;
@@ -31,10 +36,20 @@ MDK_BackgroundTask* MDK_BackgroundTask_create(void (*taskMain)(void* data), void
 }
 
 void MDK_BackgroundTask_destroy(MDK_BackgroundTask* task) {
-  pthread_cancel(task->thread);
+  if (!task->isRunning) {
+    pthread_cancel(task->thread);
+  }
+  
   free(task);
 }
 
+bool MDK_BackgroundTask_getIsRunning(MDK_BackgroundTask* task) {
+  return task->isRunning;
+}
+
 void MDK_BackgroundTask_stop(MDK_BackgroundTask* task) {
-  pthread_cancel(task->thread);
+  if (!task->isRunning) {
+    pthread_cancel(task->thread);
+    task->isRunning = false;
+  }
 }
