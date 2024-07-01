7f953821482878b85a491795cae2c7523b3d8c1c
circl <circl.lastname@gmail.com>
2024-07-01 17:48:56 +0200

    libMTK: Link with libxcb and connect to X11

--- src/libMTK/WindowManagerBackend/X11.c
+++ src/libMTK/WindowManagerBackend/X11.c
@@ -1,5 +1,19 @@
+#include <stdlib.h>
+#include <xcb/xcb.h>
+
 #include <MTK/WindowManagerBackend/X11.h>
 
 MDK_Result MTK_WindowManagerBackend_X11_create(MTK_WindowManager** windowManager) {
-  return MDK_Result_genericFailure;
+  *windowManager = malloc(sizeof(MTK_WindowManager));
+  
+  xcb_connection_t* x11Connection = xcb_connect(NULL, NULL);
+  (*windowManager)->data = x11Connection;
+  
+  if (xcb_connection_has_error(x11Connection) > 0) {
+    xcb_disconnect(x11Connection);
+    free(*windowManager);
+    return MDK_Result_genericFailure;
+  }
+  
+  return MDK_Result_success;
 }
