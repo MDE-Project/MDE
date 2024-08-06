935c20b756b4d1de2ba651ed1b3a62476aa86b9a
circl <circl.lastname@gmail.com>
2024-08-06 14:07:58 +0200

    libMDK: Add shorthand for event handler cast

--- include/MDK/Shorthand.h
+++ include/MDK/Shorthand.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <MDK/Event.h>
 #include <MDK/Object.h>
 
 #define OBJ(object) (MDK_Object*)object
 #define OBJ_CREATE(type) (type*)MDK_Object_create((MDK_Object_Destructor)destructor, sizeof(type))
 #define REF(object) MDK_Object_ref((MDK_Object*)object)
 #define UNREF(object) MDK_Object_unref((MDK_Object*)object)
+
+#define HND(handler) (MDK_Event_Handler)handler
