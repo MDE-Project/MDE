04dddb2208ebb479e4ad2eafd70f8dc542cd6d7a
circl <circl.lastname@gmail.com>
2024-08-06 13:56:22 +0200

    libMDK: Add new Event object

---
+++ include/MDK/Event.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <MDK/Object.h>
+#include <MDK/TypeSig.h>
+
+#define MDK_Event_createHandlerType(type) \
+  typedef void (*type##_Handler)(MDK_Object* this, type* event)
+
+typedef struct MDK_Event_struct MDK_Event;
+MDK_TypeSig_create(MDK_Event, 'E', 'v', 'e', 'n');
+
+MDK_Event_createHandlerType(MDK_Event);
+
+struct MDK_Event_struct {
+  MDK_Object inherited;
+  MDK_TypeSig sig;
+  
+  MDK_Object* source;
+  MDK_Object* target;
+  MDK_Event_Handler handler;
+};
+
+MDK_Event* MDK_Event_create(MDK_Object* source, MDK_Object* target, MDK_Event_Handler handler);
+void MDK_Event_init(MDK_Event* this, MDK_Object* source, MDK_Object* target, MDK_Event_Handler handler);
