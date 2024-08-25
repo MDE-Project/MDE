2125836b46f617751b243c19502704cc28ec3013
circl <circl.lastname@gmail.com>
2024-08-25 13:03:58 +0200

    libMTK: Make WindowManager_Wayland_create failable and connect to display

--- Source/libMTK/WindowManager/Wayland.c
+++ Source/libMTK/WindowManager/Wayland.c
@@ -1,20 +1,34 @@
+#include <wayland-client-core.h>
+#include <wayland-client-protocol.h>
+
 #include <MDK/Object.h>
+#include <MDK/Result.h>
 #include <MDK/Shorthand.h>
 #include <MTK/WindowManager.h>
 #include <MTK/WindowManager/Wayland.h>
 
-MTK_WindowManager_Wayland* MTK_WindowManager_Wayland_create() {
-  MTK_WindowManager_Wayland* this = OBJ_CREATE(MTK_WindowManager_Wayland);
-  MTK_WindowManager_Wayland_init(this);
-  return this;
+MDK_Result MTK_WindowManager_Wayland_create(MTK_WindowManager_Wayland** this) {
+  *this = OBJ_CREATE(MTK_WindowManager_Wayland);
+  return MTK_WindowManager_Wayland_init(*this);
 }
 
-void MTK_WindowManager_Wayland_init(MTK_WindowManager_Wayland* this) {
+MDK_Result MTK_WindowManager_Wayland_init(MTK_WindowManager_Wayland* this) {
   MTK_WindowManager_init(&this->inherited);
   this->id = MTK_WindowManager_Wayland_typeID;
+  
+  this->display = wl_display_connect(NULL);
+  if (!this->display) {
+    return MDK_Result_genericFailure;
+  }
+  
+  return MDK_Result_success;
 }
 
 void MTK_WindowManager_Wayland_destroy(MTK_WindowManager_Wayland* this) {
   ENSURE(MTK_WindowManager_Wayland);
   MTK_WindowManager_destroy(&this->inherited);
+  
+  if (this->display) {
+    wl_display_disconnect(this->display);
+  }
 }
--- include/MTK/WindowManager/Wayland.h
+++ include/MTK/WindowManager/Wayland.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #include <MDK/Object.h>
+#include <MDK/Result.h>
 #include <MDK/TypeID.h>
 #include <MTK/WindowManager.h>
 
 MDK_TypeID_create(MTK_WindowManager_Wayland, 'W', 'M', 'W', 'a');
 
+// Opaque Wayland types
+struct wl_display;
+
 typedef struct {
   MTK_WindowManager inherited;
   MDK_TypeID id;
+  
+  struct wl_display* display;
 } MTK_WindowManager_Wayland;
 
-MTK_WindowManager_Wayland* MTK_WindowManager_Wayland_create();
-void MTK_WindowManager_Wayland_init(MTK_WindowManager_Wayland* this);
+MDK_Result MTK_WindowManager_Wayland_create(MTK_WindowManager_Wayland** this);
+MDK_Result MTK_WindowManager_Wayland_init(MTK_WindowManager_Wayland* this);
 void MTK_WindowManager_Wayland_destroy(MTK_WindowManager_Wayland* this);
